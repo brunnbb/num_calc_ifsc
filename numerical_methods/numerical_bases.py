7b1e8b17d9ecf4f059167de27c1cc67800f91de3
Bruno <brunobbianchini@gmail.com>
2024-12-03 13:03:19 -0300

    Fixed numerical bases methods

--- numerical_methods/numerical_bases.py
+++ numerical_methods/numerical_bases.py
@@ -1,36 +1,22 @@
-#To review / Incomplete
+def binToDeci(binary: str) -> float:
+  binary_parts: list[str] = binary.split(".")
+  integer_part: str = binary_parts[0]
+  fractional_part: str = binary_parts[1] if len(binary_parts) > 1 else ""
 
-def binToDeci(binary : float) -> float:
-
-  binary_str = str(binary)
-  binary_array = binary_str.split(".")
-
-  n = len(binary_array[0]) - 1
-
-  decimal = 0
-  for x in binary_str:
-    if x == '.':
-      continue
-    decimal += float(x) *(2 ** n)
-    n -= 1
-
+  decimal: float = sum(int(bit) * (2**i) for i, bit in enumerate(reversed(integer_part)))
+  decimal += sum(int(bit) * (2 ** -(i + 1)) for i, bit in enumerate(fractional_part))
   return decimal
 
-def deciToBin(dec: int) -> str:
-    if dec == 0:
-        return "0"
+def deciToBin(decimal: str) -> str:
+  decimal_parts: list[str] = decimal.split(".")
+  integer_part = int(decimal_parts[0])
+  fractional_part: float = float("0." + decimal_parts[1]) if len(decimal_parts) > 1 else 0.0
 
-    bin_array = []
-    quotient = dec
-
-    while quotient > 0:
-        rest = quotient % 2
-        bin_array.append(str(rest))
-        quotient //= 2
-
-    bin_array.reverse()
-
-    bin_str = "".join(bin_array)
-    return bin_str
-
-print(binToDeci(11.11))
+  binary_integer: str = bin(integer_part)[2:]
+  binary_fractional: list[str] = []
+  while fractional_part > 0 and len(binary_fractional) < 64:
+      fractional_part *= 2
+      bit = int(fractional_part)
+      binary_fractional.append(str(bit))
+      fractional_part -= bit
+  return (f"{binary_integer}.{''.join(binary_fractional)}" if binary_fractional else binary_integer)
