ef37c4f6bfb8842bb24febb2aba7f3075c4a97c7
Bruno <brunobbianchini@gmail.com>
2025-02-15 21:20:02 -0300

    Added new graph for data and function

--- src/numerical_methods/graph_plotting.py
+++ src/numerical_methods/graph_plotting.py
@@ -49,3 +49,19 @@ def plot_tan_function(expr, interval : tuple):
     plt.grid(True)
     plt.ylim([-1, 1])
     plt.show()
+
+def plot_points_and_function(xi: np.ndarray, yi: np.ndarray, poly: callable): # type: ignore
+    x_vals = np.linspace(min(xi) - 1, max(xi) + 1, 500)
+    y_vals = poly(x_vals)
+    
+    plt.plot(x_vals, y_vals, label="Polynomial function", color='blue')
+    plt.scatter(xi, yi, color='red', label="Data Points", zorder=5)
+    
+    plt.axhline(0, color='black', linewidth=2)  
+    plt.axvline(0, color='black', linewidth=2) 
+    
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
